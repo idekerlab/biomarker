b5f501286553fcf764b510b39b8225d3067e9c14
Keiichiro Ono <keiono@gmail.com>
2012-09-24 17:01:38 -0700

    Tests added.

---
+++ src/test/java/org/cytoscape/biomarker/AbstractFinderAlgorithmTest.java
@@ -0,0 +1,24 @@
+package org.cytoscape.biomarker;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public abstract class AbstractFinderAlgorithmTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAlgorithm() {
+		// Add tests here.
+	}
+
+}
